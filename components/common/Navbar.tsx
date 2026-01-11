e4cdd519979130063b41c5633ee24160c4b07d2e
umar7shaikh <umarskpune@gmail.com>
2026-01-12 01:18:38 +0530

    fixed the prod error for the navbar

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -105,7 +105,7 @@ export default function Navbar() {
               >
                 {link.label}
               </Link>
-            ))
+            ))}
             
             {/* Vertical Divider */}
             <div 
